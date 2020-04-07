e875ca4589cb4c61d3c96c0cd702c6936dfa54c4
alex.branavitckiy@gmail.com <alex.branavitckiy@gmail.com>
2020-04-07 10:34:27 +0300

    add spring dependencis and scan package

---
+++ backend/src/main/java/packagScan/BackendApp.java
@@ -0,0 +1,13 @@
+package packagScan;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApp {
+    public static void main(String arg[]) {
+        SpringApplication.run(BackendApp.class, arg);
+    }
+}
+
---
+++ fapi/src/main/java/packagScan/FapiApp.java
@@ -0,0 +1,12 @@
+package packagScan;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FapiApp {
+    public static void main(String arg[]) {
+        SpringApplication.run(FapiApp.class, arg);
+    }
+}
